1008e4f70457edc2b952621c4a340060907a03d3
Milan Milas <Milan.Milas@sequoiacapital.co.uk>
2015-03-29 15:44:14 +0100

    simple data grid with color converter

---
+++ GridSortingWithColorConverter/PeopleViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GridSortingWithColorConverter
+{
+	public class PeopleViewModel
+	{
+		public ObservableCollection<Person> People { get; set; }
+		public ObservableCollection<Person> People2 { get; set; }
+
+		public PeopleViewModel()
+		{
+			People = new ObservableCollection<Person>(
+				new List<Person>
+					{
+						new Person() {Age = 5, Name = "Dragan"},
+						new Person() {Age = 25, Name = "Ana"},
+						new Person() {Age = 65, Name = "Milica"}
+					}
+				);
+	
+			People2 = new ObservableCollection<Person>(
+				new List<Person>
+					{
+						new Person() {Age = 5, Name = "Dragan2"},
+						new Person() {Age = 25, Name = "Ana2"},
+						new Person() {Age = 65, Name = "Milica2"}
+					}
+				);
+		}
+	}
+}
