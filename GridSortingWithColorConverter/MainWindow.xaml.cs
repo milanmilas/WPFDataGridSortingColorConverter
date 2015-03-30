8c682fe1dc49c677d5ebbc82dd3852cab3a190a5
Milan Milas <milanmilas@gmail.com>
2015-03-30 21:10:50 +0100

    Fixed DataGrid issue reproduced

--- GridSortingWithColorConverter/MainWindow.xaml.cs
+++ GridSortingWithColorConverter/MainWindow.xaml.cs
@@ -34,6 +34,22 @@ namespace GridSortingWithColorConverter
 		}
 	}
 
+    public class SimpleBrushColorConverter : IValueConverter
+    {
+        public object Convert(object values, Type targetType, object parameter, CultureInfo culture)
+        {
+            Debug.WriteLine("Simple Converter Called: " + values + " , " + values);
+            if ((int)values < 18) return new SolidColorBrush(Color.FromRgb(250, 0, 0));
+            if (60 > (int)values && (int)values > 18) return new SolidColorBrush(Color.FromRgb(0, 250, 0));
+            return new SolidColorBrush(Color.FromRgb(0, 0, 250));
+        }
+
+        public object ConvertBack(object value, Type targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
 	/// <summary>
 	/// Interaction logic for MainWindow.xaml
 	/// </summary>
@@ -97,5 +113,12 @@ namespace GridSortingWithColorConverter
 			Debug.WriteLine("Age Incremented");
 			vm.People.First().Age++;
 		}
+
+        private void OnCellMouseLeftButtonDown2(object sender, MouseButtonEventArgs e)
+        {
+            Debug.WriteLine("");
+            Debug.WriteLine("Age2 Incremented");
+            vm.People2.First().Age++;
+        }
 	}
 }
